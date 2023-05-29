26330de4ecd211caa89a092818f9ffc24da3f63f
alfandi <fandidixpc1@gmail.com>
2023-05-29 15:56:19 +0700

    Program polimorpi selesai

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class seseorang {
 public:
-	virtual void pesan() 0;
+	virtual void pesan() = 0;
 	//virtual void pesan(){
 	//cout << "pesan dari seseorang" << endl;
 	//}
@@ -11,8 +11,28 @@ public:
 
 class joko :public seseorang {
 public:
-	void pesan() {
+	void pesan() { 
 		cout << "Pesan dari joko" << endl;
 	}
 };
 
+class lia :public seseorang {
+public:
+	void pesan() {
+		cout << "pesan dari lia" << endl;
+	}
+
+};
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+	//b.seseorang::pesan();
+}
