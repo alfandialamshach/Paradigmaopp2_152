4e0da09a77d8b99b4b175301df605f9ec9140c81
alfandi <fandidixpc1@gmail.com>
2023-05-29 18:58:04 +0700

    Program no pewarisan selesai

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -7,3 +7,15 @@ public:
 		cout << "Hello saya Fuction dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << " Hello saya Function dari Derived class";
+	}
+};
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
