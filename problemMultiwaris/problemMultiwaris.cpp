07fb9a0090c53af49d49fd5001454c6bf1f39b56
alfandi <fandidixpc1@gmail.com>
2023-05-29 15:23:37 +0700

    membuat class pelajar

--- problemMultiwaris/problemMultiwaris.cpp
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -5,13 +5,25 @@ class orang {
 public:
 	int umur;
 
-	orang(input pUmur) :
+	orang(int pUmur) :
 		umur(pUmur)
 	{
-		cout << "orang dibuat dengan umur " << umur << "\n" endl;
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
 	}
 };
 
 class pekerja : public orang {
+public:
 
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : public orang {
+public:
+
+	pelajar(int pUmur) :
 };
